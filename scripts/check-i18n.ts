031f8be2e1621064164cd4fb3d5f8f05d838a730
1600822305 <1600822305@qq.com>
2025-04-11 03:46:20 +0800

    修复zhcn

--- scripts/check-i18n.ts
+++ scripts/check-i18n.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 const translationsDir = path.join(__dirname, '../src/renderer/src/i18n/locales')
-const baseLocale = 'zh-CN'
+const baseLocale = 'zh-cn'
 const baseFileName = `${baseLocale}.json`
 const baseFilePath = path.join(translationsDir, baseFileName)
 
